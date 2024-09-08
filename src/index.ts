ccd5e007335b391c024212e5119273cfe7c2531a
Jason Godesky <jefgodesky@gmail.com>
2024-09-08 15:25:41 -0400

    Render map properly, set a nice zoom level

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { map, latLng, latLngBounds, imageOverlay, MapOptions } from 'leaflet'
+const bounds = latLngBounds(
+  latLng(-90, -180),
+  latLng(90, 180)
+)
+
 
 const options: MapOptions = {
+  crs: CRS.EPSG4326,
   center: latLng(0, 0),
-  zoom: 12
+  zoom: 3,
+  minZoom: 3,
+  maxZoom: 7,
+  maxBounds: bounds,
+  maxBoundsViscosity: 1
 }
 
 const avar = map('map', options)
-const bounds = latLngBounds(latLng(-90, -180), latLng(90, 180))
 imageOverlay('map.png', bounds).addTo(avar)
 avar.fitBounds(bounds)
