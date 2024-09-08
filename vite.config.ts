c091e12a2e31abdfd4941937a7678f4a8e1fd958
Jason Godesky <jefgodesky@gmail.com>
2024-09-08 17:59:15 -0400

    Fix Vite configuration for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
   },
   server: {
     open: true,
